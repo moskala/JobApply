030339247596f208d8ca25b2fd4cf6cc258d1b07
moskala <moskal.ala@gmail.com>
2019-10-17 09:47:35 +0200

    Lab2: displaying all job offers and job details, nav bar

---
+++ JobApply/JobApply/Models/JobOffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApply.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
